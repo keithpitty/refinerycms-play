2a2a2407795eca0c402bedd6b36eb5e8e15cb862
Keith Pitty <keith@keithpitty.com>
2012-02-20 21:41:32 +1100

    Created refinerycms app from edge.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefinerycmsPlay::Application.config.secret_token = 'f4bcb760ac0c4fe97b2d449bef0411d6530f863664cfb0b33c73bc5e8f1e9551efa1c4be6838b39d2668637a515253755d6f3e1cd04bfce5b1fd52fab5982a11'
